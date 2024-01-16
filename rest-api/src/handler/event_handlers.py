80cc0d5de2e3bafdc9cd7e006ce5e819b3c21ed3
ArtemDzhaga <82814261+ArtemDzhaga@users.noreply.github.com>
2024-01-16 20:04:50 +0300

    event_handlers.py

--- rest-api/src/handler/event_handlers.py
+++ rest-api/src/handler/event_handlers.py
@@ -5,14 +5,22 @@ from utils.mongo_manager import MongoDBManager
 from utils.elasticsearch_utils import ElsaticSearchManager
 
 
+# выполняется сразу при запуске приложения
 async def startup():
+    # запускает асинхронную инициализацию клиента MongoDB, возвращая объект future
     init_mongo_future = MongoDBManager.init_mongo_client()
+    # запускает асинхронное подключение и инициализацию Elasticsearch, возвращая объект Future
     init_elasticsearch_future = ElsaticSearchManager.connect_and_init_elasticsearch()
+    # ожидает завершения обоих асинхронных операций, обеспечивая их параллельное выполнение. Что-то из разряда мониторов в С++
     await asyncio.gather(init_mongo_future, init_elasticsearch_future)
+    # инициализирует клиент Memcached, операция синхронная
     MemcachedManager.init_memcached_client()
 
-
+# что происходит при остановке
 async def shutdown():
+    # закрывает соединение с MongoDB
     MongoDBManager.close_connection()
+    # закрывает соединение с Memcached.
     MemcachedManager.close_memcached_connect()
+    # закрывает соединение с Elasticsearch, но операция асинхронная
     await ElsaticSearchManager.close_elasticsearch_connect()
